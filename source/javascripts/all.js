e61d75c3c8dcfd18efc75cb54899020ec9717467
Victor Delannoy <victor.delannoy@gmail.com>
2016-10-31 15:47:36 +0100

    first site version

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+<!-- Including Bootstrap JS (with its jQuery dependency) so that dynamic components work -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
