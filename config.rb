8a1ff14503a688bcc6986ed485aa1a05d355aef2
Victor Delannoy <victor.delannoy@gmail.com>
2016-11-01 17:38:37 +0100

    static not full

--- config.rb
+++ config.rb
@@ -30,6 +30,10 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+["seb", "romain", "anne"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { owner: name }, :ignore = true
+end
+
 
 ###
 # Helpers
